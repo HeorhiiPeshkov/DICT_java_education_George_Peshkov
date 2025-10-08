7631eff1425d2e9d1ad4ab120da0e5fc14c8b402
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2025-10-08 14:44:37 +0300

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,15 @@
-public class ChatBot {
+import java.util.Scanner;
+
+public class ChatBot{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String BotName = "Khai_Chat_Bot";
+        int BirthYear = 2025;
+        System.out.println("Hello! My name is " + BotName + "!");
+        System.out.println("I was born in " + BirthYear + "!");
+        System.out.println("Please, remind me your name.");
+        String YourName = scanner.nextLine();
+        System.out.println("What a great name you have, " + YourName + "!");
+
+    }
 }
