9ca1e636854d570b59708ee63c7cd747a5db5f07
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2025-10-12 20:34:13 +0300

    Hangman 3rd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
+import java.util.Random;
+
+
 
 public class Hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN"+ "\n" + "Guess the word:>");
         Scanner scanner = new Scanner(System.in);
-        String python = "Python";
+        Random random = new Random();
+        List<String> programming_languages = new ArrayList<>();
+        programming_languages.add("java");
+        programming_languages.add("python");
+        programming_languages.add("javascript");
+        programming_languages.add("kotlin");
+        String random_word = programming_languages.get(random.nextInt(programming_languages.size()));
+        System.out.println("HANGMAN"+ "\n" + "Guess the word:>" + "(" + random_word + ")");
         String userChoice = scanner.nextLine();
-        if (userChoice.equalsIgnoreCase(python)) {
+        if (userChoice.equalsIgnoreCase(random_word)) {
             System.out.println("You Won");
         } else {
             System.out.println("You Lost");
