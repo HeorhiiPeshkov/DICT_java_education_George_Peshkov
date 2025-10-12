c60bb094b5fb32253a2fde09aae01ae0858e0f2a
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2025-10-12 22:25:28 +0300

    Hangman 5th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -7,26 +7,50 @@ public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        int attempts = 0;
+        int maxAttempts = 8;
+
         List<String> programming_languages = new ArrayList<>();
-        programming_languages.add("ja--");
-        programming_languages.add("py----");
-        programming_languages.add("ja--------");
-        programming_languages.add("ko----");
-        List<String> unhidden_programming_languages = new ArrayList<>();
-        unhidden_programming_languages.add("java");
-        unhidden_programming_languages.add("python");
-        unhidden_programming_languages.add("javascript");
-        unhidden_programming_languages.add("kotlin");
+        programming_languages.add("java");
+        programming_languages.add("python");
+        programming_languages.add("javascript");
+        programming_languages.add("kotlin");
+
+
         int randomIndex = random.nextInt(programming_languages.size());
-        String targetWord = unhidden_programming_languages.get(randomIndex);
-        String currentWord = programming_languages.get(randomIndex);
-        System.out.println("HANGMAN"+ "\n" + "Guess the word:>" + currentWord);
-        String userChoice = scanner.nextLine();
-        if (userChoice.equalsIgnoreCase(targetWord)) {
-            System.out.println("You Won");
-        } else {
-            System.out.println("You Lost");
+        String rightWord = programming_languages.get(randomIndex);
+
+        char[] hiddenWord = new char[rightWord.length()];
+        for (int i = 0; i < hiddenWord.length; i++) {
+            hiddenWord[i] = '-';
         }
-        scanner.close();
+
+        while (attempts < maxAttempts) {
+            System.out.println("HANGMAN" + "\n" + String.valueOf(hiddenWord));
+            String input = scanner.nextLine();
+            char guessedLetter = input.charAt(0);
+            boolean found = false;
+            for (int i = 0; i < hiddenWord.length; i++) {
+                if (rightWord.charAt(i) == guessedLetter && hiddenWord[i] == '-') {
+                    hiddenWord[i] = guessedLetter;
+                    found = true;
+            }
+        }
+        if (found) {
+            System.out.println("Right!");
+        } else {
+            System.out.println("Wrong!");
+            attempts++;
+        }
+        if (String.valueOf(hiddenWord).equals(rightWord)) {
+            System.out.println("You win!");
+            break;
+        }
+    }
+    if (attempts == maxAttempts) {
+        System.out.println("You lost!");
+    }
+    scanner.close();
     }
 }
