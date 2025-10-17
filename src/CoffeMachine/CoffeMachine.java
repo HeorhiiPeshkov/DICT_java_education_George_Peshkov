3106e5b1f111c17a5e164a893b8daa5b4b41ff1d
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2025-10-17 16:22:07 +0300

    CoffeMachie 3rd stage

--- src/CoffeMachine/CoffeMachine.java
+++ src/CoffeMachine/CoffeMachine.java
@@ -1,19 +1,34 @@
 package CoffeMachine;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CoffeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:" + "\n" + ">");
+        int has_water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:" + "\n" + ">");
+        int has_milk = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:" + "\n" + ">");
+        int has_coffee = scanner.nextInt();
         System.out.println("Write how many cups of coffee you will need:" + "\n" + ">");
-        int cups = scanner.nextInt();
-        System.out.println("For " + cups + " cups of coffee you will need");
-        int water = 200;
-        int milk = 50;
-        int beans = 75;
-        System.out.println(water * cups + " ml of water");
-        System.out.println(milk * cups + " ml of milk");
-        System.out.println(beans * cups + " ml of beans");
+        int caps_coffee = scanner.nextInt();
+        int water = caps_coffee * 200;
+        int milk = caps_coffee * 50;
+        int beans = caps_coffee * 75;
+        int cups_coffee_can_make1 = has_water / 200;
+        int cups_coffee_can_make2 = has_milk / 50;
+        int cups_coffee_can_make3 = has_coffee / 75;
+        List<Integer> array_cups_coffee_can_make = new ArrayList<>();
+        array_cups_coffee_can_make.add(cups_coffee_can_make1);
+        array_cups_coffee_can_make.add(cups_coffee_can_make2);
+        array_cups_coffee_can_make.add(cups_coffee_can_make3);
+        int max_cups = Collections.min(array_cups_coffee_can_make);
+        if (max_cups == caps_coffee) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            System.out.println("Sorry, we don't have enough" + "\n" + "I can make only " + max_cups + " cups of coffee");
+        }
         scanner.close();
     }
 }
