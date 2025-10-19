049ec6b11e912f9873b987eeb22c76cfa9965039
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2025-10-19 10:56:18 +0300

    TicTacToe 3rd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -11,5 +11,30 @@ public class TicTacToe {
         System.out.println("| " + cells.charAt(3) + "" + cells.charAt(4) + "" + cells.charAt(5) + "|" + "\n");
         System.out.println("| " + cells.charAt(6) + "" + cells.charAt(7) + "" + cells.charAt(8) + "|" + "\n");
         System.out.println("---------");
+        if (cells.length() != 9) {
+            System.out.println("Game not finished");
+            return;
+        }
+        if  (cells.charAt(0) == 'X' && cells.charAt(1) == 'X' && cells.charAt(2) == 'X'||
+            cells.charAt(0) == 'X' && cells.charAt(3) == 'X' && cells.charAt(6) == 'X' ||
+            cells.charAt(1) == 'X' && cells.charAt(4) == 'X' && cells.charAt(7) == 'X'||
+            cells.charAt(2) == 'X' && cells.charAt(5) == 'X' && cells.charAt(8) == 'X'||
+            cells.charAt(6) == 'X' && cells.charAt(7) == 'X' && cells.charAt(8) == 'X'||
+            cells.charAt(3) == 'X' && cells.charAt(4) == 'X' && cells.charAt(5) == 'X'||
+            cells.charAt(0) == 'X' && cells.charAt(4) == 'X' && cells.charAt(8) == 'X'||
+            cells.charAt(2) == 'X' && cells.charAt(4) == 'X' && cells.charAt(6) == 'X') {
+            System.out.println("X win");
+        } else if (cells.charAt(0) == 'O' && cells.charAt(1) == 'O' && cells.charAt(2) == 'O'||
+                cells.charAt(0) == 'O' && cells.charAt(3) == 'O' && cells.charAt(6) == 'O' ||
+                cells.charAt(1) == 'O' && cells.charAt(4) == 'O' && cells.charAt(7) == 'O'||
+                cells.charAt(2) == 'O' && cells.charAt(5) == 'O' && cells.charAt(8) == 'O'||
+                cells.charAt(6) == 'O' && cells.charAt(7) == 'O' && cells.charAt(8) == 'O'||
+                cells.charAt(3) == 'O' && cells.charAt(4) == 'O' && cells.charAt(5) == 'O'||
+                cells.charAt(0) == 'O' && cells.charAt(4) == 'O' && cells.charAt(8) == 'O'||
+                cells.charAt(2) == 'O' && cells.charAt(4) == 'O' && cells.charAt(6) == 'O') {
+            System.out.println("O win");
+        } else {
+            System.out.println("Impossible");
+        }
     }
 }
